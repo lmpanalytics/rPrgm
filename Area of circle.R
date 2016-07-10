8db2ab760271561dc5b1c29e6080b95531e5c38d
lmpanalytics <lmpconsulting@hotmail.com>
2016-07-10 11:47:51 +0200

    Added plot of cirle area

--- Area of circle.R
+++ Area of circle.R
@@ -2,3 +2,12 @@
 setwd("~/Magnus documents/R test")
 #This is the area function to be used
 area <- function(r){pi * r^2}
+
+values <- 5
+y <- double(length(values))
+for (i in 1:values) {
+  y = c(y,area(i))
+}
+
+plot(x=c(0:5),y,type = "b", xlab ="radius", ylab = "area")
+rm(x,y)
