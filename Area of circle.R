6da54d0bd1aea56c69934688080e0fc42eb60b11
lmpanalytics2 <lmpconsulting@hotmail.com>
2016-07-10 13:40:55 +0200

    Added comment and removed rm command from R file

--- Area of circle.R
+++ Area of circle.R
@@ -2,7 +2,7 @@
 setwd("~/Magnus documents/R test")
 #This is the area function to be used
 area <- function(r){pi * r^2}
-
+# Preset the number of different radiuses to plot
 values <- 5
 y <- double(length(values))
 for (i in 1:values) {
@@ -10,4 +10,3 @@ for (i in 1:values) {
 }
 
 plot(x=c(0:5),y,type = "b", xlab ="radius", ylab = "area")
-rm(x,y)
